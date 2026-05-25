80f3fdb2bb2d1dfe2df3eca89f2dfd49eff17332
Yen-Chung Chen <chenyenjung@gmail.com>
2026-05-25 11:09:25 -0400

    Vsx1-LOF-Tm5e analysis (Currently binarize to Ey-Kn)
    - Try to do with a makefile

--- Vsx1-LOF-Tm5e/get_dbp.sh
+++ Vsx1-LOF-Tm5e/get_dbp.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+set -e
 
 for i in 1 2 3; do
+  echo "Processing mCherry_${i} for batch 1..."
   micromamba run -n tiff python ../get_positive.py \
-    -i batch1/mCherryi${i}_D_Ey_Kn.tif \
+    -i batch1/mCherryi_${i}_D_Ey_Kn.tif \
     -m batch1/mCherryi_${i}_filtered.tif \
     -t 1000 625 -c 1 2 \
     -o batch1/mCherryi_${i}_eykn.tif
 done
 for i in 1 2 3 4; do
+  echo "Processing VKD_${i} for batch 1..."
   micromamba run -n tiff python ../get_positive.py \
     -i batch1/VKD_${i}_D_Ey_Kn.tif \
     -m batch1/VKD_${i}_filtered.tif \
@@ -15,16 +18,18 @@ for i in 1 2 3 4; do
     -o batch1/VKD_${i}_eykn.tif
 done
 for i in 1 2 3; do
+  echo "Processing mCherry_${i} for batch 2..."
   micromamba run -n tiff python ../get_positive.py \
-    -i batch2/mCherryi${i}_D_Ey_Kn.tif \
+    -i batch2/mCherryi_${i}_D_Ey_Kn.tif \
     -m batch2/mCherryi_${i}_filtered.tif \
     -t 1000 625 -c 1 2 \
     -o batch2/mCherryi_${i}_eykn.tif
 done
 for i in 1 2 3; do
+  echo "Processing VKD_${i} for batch 2..."
   micromamba run -n tiff python ../get_positive.py \
     -i batch2/VKD_${i}_D_Ey_Kn.tif \
     -m batch2/VKD_${i}_filtered.tif \
     -t 1000 625 -c 1 2 \
-    -o batch1/VKD_${i}_eykn.tif
+    -o batch2/VKD_${i}_eykn.tif
 done
