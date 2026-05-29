e2f0336d1e723cd337960517a51cbb66faf18cff
Yen-Chung Chen <chenyenjung@gmail.com>
2026-05-29 14:54:19 -0400

    Tighten the threshold for Kn

--- Vsx1-LOF-Tm5e/get_dbp.sh
+++ Vsx1-LOF-Tm5e/get_dbp.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 set -e
 
+EY_THRES=800
+KN_THRES=800
+
 for i in 1 2 3; do
   echo "Processing mCherry_${i} for batch 1..."
   micromamba run -n tiff python ../get_positive.py \
     -i batch1/mCherryi_${i}_D_Ey_Kn.tif \
     -m batch1/mCherryi_${i}_filtered.tif \
-    -t 1000 625 -c 1 2 \
+    -t ${EY_THRES} ${KN_THRES} -c 1 2 \
     -o batch1/mCherryi_${i}_eykn.tif
 done
-for i in 1 2 3 4; do
+for i in 2 3 4; do
   echo "Processing VKD_${i} for batch 1..."
   micromamba run -n tiff python ../get_positive.py \
     -i batch1/VKD_${i}_D_Ey_Kn.tif \
     -m batch1/VKD_${i}_filtered.tif \
-    -t 1000 625 -c 1 2 \
+    -t ${EY_THRES} ${KN_THRES} -c 1 2 \
     -o batch1/VKD_${i}_eykn.tif
 done
-for i in 1 2 3; do
+for i in 2 3; do
   echo "Processing mCherry_${i} for batch 2..."
   micromamba run -n tiff python ../get_positive.py \
     -i batch2/mCherryi_${i}_D_Ey_Kn.tif \
     -m batch2/mCherryi_${i}_filtered.tif \
-    -t 1000 625 -c 1 2 \
+    -t ${EY_THRES} ${KN_THRES} -c 1 2 \
     -o batch2/mCherryi_${i}_eykn.tif
 done
 for i in 1 2 3; do
@@ -30,6 +33,6 @@ for i in 1 2 3; do
   micromamba run -n tiff python ../get_positive.py \
     -i batch2/VKD_${i}_D_Ey_Kn.tif \
     -m batch2/VKD_${i}_filtered.tif \
-    -t 1000 625 -c 1 2 \
+    -t ${EY_THRES} ${KN_THRES} -c 1 2 \
     -o batch2/VKD_${i}_eykn.tif
 done
